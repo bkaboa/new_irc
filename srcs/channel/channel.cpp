6f2454af184a3c1e34a659dcc1b3d0cc62c4bbc4
bkaboa <charleszang5@gmail.com>
2023-05-23 20:55:35 +0200

    some change

--- srcs/channel/channel.cpp
+++ srcs/channel/channel.cpp
@@ -10,6 +10,13 @@ const std::string	&Channel::getName() const {
 	return (_Name);
 }
 
-bool	Channel::checkBan(std::string name) {
-	if ()
+bool	Channel::checkBan(fd_t clientFd) {
+	vectorFdClientIter vIter = _FdClientBanned.begin();
+
+	for (;vIter != _FdClientBanned.end(); vIter++)
+	{
+		if (clientFd == *vIter.base())
+			return (false);
+	}
+	return (true);
 }
