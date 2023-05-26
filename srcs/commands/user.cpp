8371763299f10779d966f49c735cba767954b3ce
lmaurin- <leonard.maurin@gmail.com>
2023-05-26 18:35:04 +0200

    added commands into Server class

--- srcs/commands/user.cpp
+++ srcs/commands/user.cpp
@@ -1,1 +1,10 @@
 #include "../../include/commands/commands.hpp"
+
+// #include "../../include/server/server.hpp"
+
+void User(fd_t sender, Server *server, std::vector<std::string> args)
+{
+	(void) sender;
+	(void) args;
+	std::cout << "plou" << std::endl;
+}
